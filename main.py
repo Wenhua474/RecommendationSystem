9de60d4f59b56c8bec860d2ecb3081728fdf3632
Wenhua474 <wenhuawang@gatech.edu>
2018-10-17 12:15:34 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from predict.predict import rating_movie, recommend_same_type_movie, recommend_your_favorite_movie, recommend_other_favorite_movie
+
+rating_movie(654, 987)
+recommend_same_type_movie(987)
+recommend_your_favorite_movie(654)
+recommend_other_favorite_movie(987)
